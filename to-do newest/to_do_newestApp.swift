fb18a74c3e274f2ba6170f80764d718e02e5f1c9
Guinter Zaffalon <143457091+GuinterZaffalon@users.noreply.github.com>
2024-09-02 17:16:46 -0300

    Initial Commit

---
+++ to-do newest/to_do_newestApp.swift
@@ -0,0 +1,17 @@
+//
+//  to_do_newestApp.swift
+//  to-do newest
+//
+//  Created by Guinter Zaffalon on 02/09/24.
+//
+
+import SwiftUI
+
+@main
+struct to_do_newestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
