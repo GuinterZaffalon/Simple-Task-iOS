e035ec7701bcfd1d0ea59da844bd0690b911979b
Guinter Zaffalon <143457091+GuinterZaffalon@users.noreply.github.com>
2024-09-14 22:36:42 -0300

    ATENÇÃO, IT`S WORKING!!!!!!

---
+++ Simple_TaskUITests/Simple_TaskUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Simple_TaskUITestsLaunchTests.swift
+//  Simple_TaskUITests
+//
+//  Created by Guinter Zaffalon on 14/09/24.
+//
+
+import XCTest
+
+final class Simple_TaskUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
--- to-do newest/SheetView.swift
+++ to-do newest/SheetView.swift
@@ -58,3 +58,5 @@ struct SheetView: View {
         }
     }
 }
+
+
